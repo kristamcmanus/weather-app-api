92d25ee6c8c673303e7114e8c432875863ac4413
kristamcmanus <k.mcmanus310@gmail.com>
2021-12-20 17:44:24 -0500

    Add Bootstrap cdn and implement Bootstrap card to display weather info

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ async function getWeatherData(e) {
 
     const userInput = $('input[type="text"]').val();
     const apiKey = "5560995d480f9bf2304ccfb709751647";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${apiKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
