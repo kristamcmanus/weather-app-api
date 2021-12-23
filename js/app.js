44a0c5d4646a976779cb62dad083bd2213faa0b0
kristamcmanus <k.mcmanus310@gmail.com>
2021-12-23 14:55:52 -0500

    Delete images and used api icons instead and update styling

--- js/app.js
+++ js/app.js
@@ -12,7 +12,26 @@ async function getWeatherData(e) {
     $("#name").html(data.name);
     $("#temp").html(data.main.temp);
     $("#desc").html(data.weather[0].description);
+    $("#icon").html(data.weather[0].icon)
     console.log(response)
+
+    // return `
+    //     <div class="card" style="width: 18rem;">
+    //     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" class="card-img-top" alt="">
+    //         <div class="card-body">
+    //             <dl id="weather-info">
+    //                 <dt>City: ${data.name}</dt>
+    //                 <dd id="name"></dd>
+    //                 <dt>Temperature: ${data.main.temp}</dt>
+    //                 <dd id="temp"></dd>
+    //                 <dt>Description: ${data.weather[0].description}</dt>
+    //                 <dd id="desc"></dd>
+    //                 <dt>Icon:</dt>
+    //                 <dd id="icon"></dd>
+    //             </dl>
+    //         </div>
+    //     </div>
+    // `
 }
 
 $("form").on("submit", getWeatherData);
